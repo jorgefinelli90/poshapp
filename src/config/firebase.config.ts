02027598c56649014650acb2dde9e03193f8c999
jorgefinelli90 <jorgefinelli90@gmail.com>
2024-12-03 12:58:49 -0300

    testing

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyB-SHZ_f2CtVp9JFZUsNcHkncFQM2DwHTI",
+  authDomain: "the-posh-app.firebaseapp.com",
+  projectId: "the-posh-app",
+  storageBucket: "the-posh-app.firebasestorage.app",
+  messagingSenderId: "841947099495",
+  appId: "1:841947099495:web:f7b723fc610da5fb3dbfc2",
+  measurementId: "G-L9FMFDXG07"
 };
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,8 +5,10 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 import { firebaseConfig } from '../config/firebase.config';
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
